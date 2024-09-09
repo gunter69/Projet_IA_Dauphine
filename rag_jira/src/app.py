9c76f99e656df0da290074aa133af062611cf0b3
mgiraud <marie10.giraud@gmail.com>
2024-09-09 19:45:41 +0200

    ajout questions pour la démo

--- rag_jira/src/app.py
+++ rag_jira/src/app.py
@@ -7,17 +7,22 @@ from langchain_community.llms.huggingface_endpoint import HuggingFaceEndpoint
 
 from service.indexation import ServiceIndexation
 
-
 st.set_page_config(page_title="JiraBot 💬🤖")
+
+def reset_conversation():
+    """Reset conversation"""
+    st.session_state.messages = [
+        {"role": "assistant", "content": "How can I help you today?"}
+    ]
 
 with st.sidebar:
     st.title("JiraBot 💬🤖")
-    st.header("Vos identifiants HF 🤗")
-    HUGGINGFACEHUB_API_TOKEN = st.text_input("🔑 Token d'API :", value="", type='password')
+    st.header("Your HF login 🤗")
+    HUGGINGFACEHUB_API_TOKEN = st.text_input("🔑 API token :", value="", type='password')
     if HUGGINGFACEHUB_API_TOKEN == '':
-        st.warning("Veuillez saisir votre token d'api HuggingFace !", icon='⚠️')
+        st.warning("Please enter your HuggingFace api token!!", icon='⚠️')
     else:
-        st.success("Votre token d'api HuggingFace a été pris en compte ! ", icon='✅')
+        st.success("Your HuggingFace api token has been taken into account! ", icon='✅')
         ## Configuration LLM
         MODEL = "mistralai/Mistral-7B-Instruct-v0.2"
         llm = HuggingFaceEndpoint(
@@ -27,20 +32,33 @@ with st.sidebar:
             huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN,
         )
 
+    st.markdown("***")
+
     st.markdown(
-        '📖 Lien vers le répertoire '
-        '[Github du projet](https://blog.streamlit.io/how-to-build-an-llm-powered-chatbot-with-streamlit/)!'
+        "📖 Link to the "
+        "[project's Github directory](https://github.com/gunter69/Projet_IA_Dauphine/tree/main/rag_jira)!"
     )
+
+    st.markdown("***")
+
+    st.markdown("**Project :** MongoDB Core Server")
+    st.markdown(
+        "**Description :** MongoDB Enterprise Server is the commercial edition of MongoDB,"
+        " available as part of the MongoDB Enterprise Advanced subscription."
+    )
+
+    st.markdown("*This chatbot is there to answer your questions about the project*")
+    st.button('Reset Chat', on_click=reset_conversation)
 
 service_indexation = ServiceIndexation()
 
 if "messages" not in st.session_state.keys():
     st.session_state.messages = [
-        {"role": "assistant", "content": "Comment puis-je vous aider aujourd'hui ?"}
+        {"role": "assistant", "content": "How can I help you today?"}
     ]
 
 if HUGGINGFACEHUB_API_TOKEN == '':
-    st.warning("Veuillez saisir votre token d'api HuggingFace 🤗 !", icon='⚠️')
+    st.warning("Please enter your HuggingFace api token!🤗 !", icon='⚠️')
 else:
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -53,9 +71,11 @@ def generate_response(prompt_input, context):
 #     template = """Tu es un assistant conversationnel qui répond en français.
 # Tu dois répondre à la question : {prompt_input}
 # Pour répondre tu dois t'aider du contexte suivant : {context}"""
-    template = """Tu es un assistant conversationnel.
-Tu dois répondre à la question : {prompt_input}
-Pour répondre tu dois t'aider du contexte suivant : {context}"""
+    template = """MongoDB Enterprise Server is the commercial edition of MongoDB, available as part of the MongoDB Enterprise Advanced subscription.
+You are a conversational assistant solving issues on MongoDB Enterprise Server.
+Answer the question: {prompt_input}
+Using the following context: {context}
+Your answer will be concise and you will explain step by step how to solve the problem."""
     prompt_temp = PromptTemplate.from_template(template)
     llm_chain = prompt_temp | llm
     return llm_chain.invoke({
@@ -73,6 +93,8 @@ if prompt := st.chat_input(placeholder="Votre question", disabled=not HUGGINGFAC
 
     CONTEXTES = ""
     for document in documents:
+        CONTEXTES += document.metadata["title"]
+        CONTEXTES += "\n"
         CONTEXTES += document.metadata["comment"]
         CONTEXTES += "\n\n"
 
